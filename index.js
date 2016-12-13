de663b59cdf9b0f6763e51be00f72b773a52b32d
Andrew Klein <ahk1212@gmail.com>
2016-12-13 07:06:08 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Andrew on 12/12/2016.
+ */
+
+var express = require('express');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get("/", function (req, res) {
+
+   res.end('hello, world 2');
+
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
